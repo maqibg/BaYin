d5cfa689ac26eec71a44e549126ffe75f54a32a5
Hao <zhao5638@gmail.com>
2026-02-01 18:23:16 +0800

    pc: persist storage for window position and size

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -16,6 +16,7 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .invoke_handler(tauri::generate_handler![
             scan_music_files,
             get_music_metadata,
